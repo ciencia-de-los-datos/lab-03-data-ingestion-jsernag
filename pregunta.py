12818286fd7745c4028e004daef95dfffc16e74f
jsernag <jsernag@unal.edu.co>
2024-04-26 15:32:40 -0500

    Intento 1

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,42 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    # df = pd.read_csv(
+    #     "clusters_report.txt",
+    #     sep='\t'
+    # )
+    archivo = "clusters_report.txt"
+    df = pd.read_fwf(
+        archivo,
+        skiprows = 4,
+        index_col = False,
+        names = [
+            "cluster",
+            "cantidad_de_palabras_clave",
+            "porcentaje_de_palabras_clave",
+            "principales_palabras_clave",
+        ]
+    )
+
+    df.cluster = df.cluster.ffill().astype(int)
+
+    df.principales_palabras_clave = df.groupby(["cluster"])["principales_palabras_clave"].transform(lambda x: " ".join(x)).str.replace(".","")
+
+    df.drop_duplicates(subset=["cluster"], inplace=True)
+
+    df.reset_index(drop=True, inplace=True)
+
+    df.cantidad_de_palabras_clave = df.cantidad_de_palabras_clave.astype(int)
+
+    df.porcentaje_de_palabras_clave = df.porcentaje_de_palabras_clave.str.replace("%","")
+    
+    df.porcentaje_de_palabras_clave = df.porcentaje_de_palabras_clave.str.replace(",",".")
+    
+    df.porcentaje_de_palabras_clave = df.porcentaje_de_palabras_clave.astype(float)
+
+    df.principales_palabras_clave = df.principales_palabras_clave.replace(r"\s+"," ",regex=True)
+
 
     return df
+
+print(ingest_data())
